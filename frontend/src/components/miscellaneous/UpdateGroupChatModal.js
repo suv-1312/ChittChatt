b44f977915776c54cee5b80826e329eed3c6581b
suv-1312 <96910024+suv-1312@users.noreply.github.com>
2023-08-16 08:20:55 +0530

    Update UpdateGroupChatModal.js

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -49,7 +49,7 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain, fetchMessages }) => {
                 },
             };
             const { data } = await axios.put(
-                'http://localhost:5000/api/chat/groupadd',
+                '/api/chat/groupadd',
                 {
                     chatId: selectedChat._id,
                     userId: user1._id,
@@ -93,7 +93,7 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain, fetchMessages }) => {
                 },
             };
             const { data } = await axios.put(
-                'http://localhost:5000/api/chat/groupremove',
+                '/api/chat/groupremove',
                 {
                     chatId: selectedChat._id,
                     userId: user1._id,
@@ -131,7 +131,7 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain, fetchMessages }) => {
                 },
             };
             const { data } = await axios.put(
-                'http://localhost:5000/api/chat/rename',
+                '/api/chat/rename',
                 {
                     chatId: selectedChat._id,
                     chatName: groupChatName,
@@ -170,7 +170,7 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain, fetchMessages }) => {
                 },
             };
 
-            const { data } = await axios.get(`http://localhost:5000/api/user?search=${search}`, config);
+            const { data } = await axios.get(`/api/user?search=${search}`, config);
             console.log(data);
             setLoading(false);
             setSearchResult(data);
